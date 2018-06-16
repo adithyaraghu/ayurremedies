6b041cebdbcca61ffdff10639f726b501a29a980
adithyaraghuraman <63vy22p0>
2018-06-16 16:03:53 -0700

    Initial upload

---
+++ src/test/java/edu/adithyar/health/ayurremedies/AyurremediesApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.adithyar.health.ayurremedies;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AyurremediesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
