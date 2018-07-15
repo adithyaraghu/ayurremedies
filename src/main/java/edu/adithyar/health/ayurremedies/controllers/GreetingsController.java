e5cfa893d32eadef025996713d90b7931ba6cf9c
adithyaraghuraman <adithya.raghu@gmail.com>
2018-07-15 11:27:28 -0700

    Adding Dockerfile - for creating images to be hosted in AWS

--- src/main/java/edu/adithyar/health/ayurremedies/controllers/GreetingsController.java
+++ src/main/java/edu/adithyar/health/ayurremedies/controllers/GreetingsController.java
@@ -11,7 +11,6 @@ public class GreetingsController {
     @GetMapping("/greetings")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greetings.html";
+        return "greetings";
     }
-
 }
